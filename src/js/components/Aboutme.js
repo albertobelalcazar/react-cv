9f4eb273472f0fa54a3fcdf16ffa8dfd19640f7f
Alberto Belalcazar <alberto.belalcazar@hotmail.com>
2021-02-12 21:23:00 +0100

    removed react strict mode

--- src/js/components/Aboutme.js
+++ src/js/components/Aboutme.js
@@ -2,33 +2,49 @@ import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import parse from "html-react-parser";
 import Mountain from './../../assets/about_mountain.jpg';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 
 export default function App() {
     const [post, setPost] = useState(null);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
-        Axios.get("http://alberto.codes/wp/wp-json/wp/v2/posts/15").then(
-            response => {
-                setPost(response.data);
-            }
-        );
+        // Esto es para comprobar si muestra el loader 
+        // const timer = setTimeout(() => {
+        //     Axios.get("http://alberto.codes/wp/wp-json/wp/v2/posts/15")
+        //         .then(res => {
+        //             setPost(res.data);
+        //         })
+        //     setLoading(true);
+        // }, 3000);
+        // return () => clearTimeout(timer);
+        //  TODO: crear un div de full screnn loader;
+
+        Axios.get("http://alberto.codes/wp/wp-json/wp/v2/posts/15")
+            .then(res => {
+                setPost(res.data);
+            })
+        setLoading(true);
+
     }, [setPost]);
 
     return (
         <div className="aboutme">
             <div className="aboutme_post">
-                {post && (
-                    <div className="aboutme__content">
-                        <div className="aboutme__content_image">
-                            <img src={Mountain} alt="about me" />
-                        </div>
-                        <div className="aboutme__content_text">
-                            <h1>{post.title.rendered}</h1>
-                            <div>{parse(post.content.rendered)}</div>
-                        </div>
+                {loading ? (post && (<div className="aboutme__content">
+                    <div className="aboutme__content_image">
+                        <img src={Mountain} alt="about me" />
                     </div>
-                )}
+                    <div className="aboutme__content_text">
+                        <h1>{post.title.rendered}</h1>
+                        <div>{parse(post.content.rendered)}</div>
+
+                    </div>
+                </div>)) : <CircularProgress />}
 
             </div>
+            {loading ? console.log(loading) : console.log(loading)}
         </div>
     );
 }
