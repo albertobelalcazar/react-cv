d16dfe8d2dd7b3dfb68485305003bea3424809fb
Alberto Belalcazar <alberto.belalcazar@hotmail.com>
2021-03-22 18:17:53 +0100

    CV start

---
+++ build/precache-manifest.d19b928ae4f373794930df05ea22514d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d89aff4327d7f7389e79984c778f2c0",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "75c474d2cff7c9a8a6d5",
+    "url": "/cv/static/css/2.cebec226.chunk.css"
+  },
+  {
+    "revision": "e457fdd0cb2cc0ef9bec",
+    "url": "/cv/static/css/main.5340ba4c.chunk.css"
+  },
+  {
+    "revision": "75c474d2cff7c9a8a6d5",
+    "url": "/cv/static/js/2.ff1feb18.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cv/static/js/2.ff1feb18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e457fdd0cb2cc0ef9bec",
+    "url": "/cv/static/js/main.e73ab78c.chunk.js"
+  },
+  {
+    "revision": "5502997e5868c921c6c8",
+    "url": "/cv/static/js/runtime-main.6064b81b.js"
+  },
+  {
+    "revision": "444e7e6739846c591b2528cc2775f136",
+    "url": "/cv/static/media/bgmountain.444e7e67.jpg"
+  },
+  {
+    "revision": "7e51471256f0f150ec593d4527f12301",
+    "url": "/cv/static/media/portfolio-2.7e514712.jpg"
+  },
+  {
+    "revision": "62ae21802b8c3eb5b86373816d89400a",
+    "url": "/cv/static/media/svedea.62ae2180.png"
+  }
+]);
