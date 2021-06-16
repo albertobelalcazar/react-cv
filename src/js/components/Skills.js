cfd60cb52d590edbd3290d567f0516087d3cc9f0
Alberto Belalcazar <alberto.belalcazar@svedea.se>
2021-06-16 16:47:00 +0200

    refactory code for Skills component

--- src/js/components/Skills.js
+++ src/js/components/Skills.js
@@ -55,56 +55,65 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
+const mySkills = [
+    {
+        id: 1,
+        icon: faHtml5,
+        title: "HTML5",
+        description: "Det är vitigt att bygga en bra struktur med alla elementer på sajten. Ja gillar att använda det senaste med HTML5",
+    },
+    {
+        id: 2,
+        icon: faCss3,
+        title: "CSS3",
+        description: "Jag älskar att bygga moderna, bygga och responsiva sajter, SASS, CSS är en viktigt del i min vardag",
+    },
+    {
+        id: 3,
+        icon: faJs,
+        title: "Javascript",
+        description: '9 + "1" = "91" eller 9 - "1" = 8 Ja, jag vet så är det med javascript :) och ändå jag gillar utveckla med det',
+    },
+    {
+        id: 4,
+        icon: faReact,
+        title: "React",
+        description: 'Det här CV:et är skapat bland annat med React, Material-UI, och flera verktig som jag använder varje dag',
+    },
+    {
+        id: 5,
+        icon: faWindows,
+        title: ".NET",
+        description: 'Blazor!  - Microsoft vill inte stana och vänta på tekniken, inte jag helller! därför jag jar jobbat med Blazor senaste året',
+    },
+    {
+        id: 6,
+        icon: faConnectdevelop,
+        title: "CMS",
+        description: 'Jag har jobbat med olika CMS system som till exempel, Wordpress, Joomla, Drupal, och flera ehandels plataformar.',
+    },
+
+]
+
+
+
 const Skills = () => {
     const classes = useStyles();
     return (
         <div className="skills">
             <div className={classes.root}>
                 <Grid container spacing={0} alignItems="center" justify="center">
-                    <Grid item xs={12} sm={6} md={4} style={{ padding: 10 }}>
-                        <Paper className={classes.paper} >
-                            <FontAwesomeIcon icon={faHtml5} style={{ color: "#356277", fontSize: 80 }} />
-                            {/* <DevicesOutlined style={{ color: "#356277", fontSize: 80 }} /> */}
-                            <h2>HTML 5</h2>
-                            <p>Det är vitigt att bygga en bra struktur med alla elementer på sajten. Ja gillar att använda det senaste med HTML5</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} style={{ padding: 10 }}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <FontAwesomeIcon icon={faCss3} style={{ color: "#356277", fontSize: 80 }} />
-                            {/* <FontAwesomeIcon icon="coffee" /> */}
-                            <h2>CSS3</h2>
-                            <p>Jag älskar att bygga moderna, bygga och responsiva sajter, SASS, CSS är en viktigt del i min vardag</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} style={{ padding: 10 }}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <FontAwesomeIcon icon={faJs} style={{ color: "#356277", fontSize: 80 }} />
-                            <h2>Javascript</h2>
-                            <p>9 + "1" = "91" eller 9 - "1" = 8 Ja, jag vet så är det med javascript :) och ändå jag gillar utveckla med det</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} style={{ padding: 10 }}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <FontAwesomeIcon icon={faReact} style={{ color: "#356277", fontSize: 80 }} />
-                            <h2>React</h2>
-                            <p>Det här CV:et är skapat bland annat med React, Material-UI, och flera verktig som jag använder varje dag</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} style={{ padding: 10 }}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <FontAwesomeIcon icon={faWindows} style={{ color: "#356277", fontSize: 80 }} />
-                            <h2>.NET</h2>
-                            <p>Blazor!  - Microsoft vill inte stana och vänta på tekniken, inte jag helller! därför jag jar jobbat med Blazor senaste året</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} style={{ padding: 10 }}>
-                        <Paper className={classes.paper} elevation={0}>
-                            <FontAwesomeIcon icon={faConnectdevelop} style={{ color: "#356277", fontSize: 80 }} />
-                            <h2>CMS</h2>
-                            <p>Jag har jobbat med olika CMS system som till exempel, Wordpress, Joomla, Drupal, och flera ehandels plataformar. </p>
-                        </Paper>
-                    </Grid>
+                    {mySkills.map((_, index) => (
+                        <Grid key={index} item xs={12} sm={6} md={4} style={{ padding: 10 }}>
+                            <Paper className={classes.paper} >
+                                <FontAwesomeIcon icon={mySkills[index].icon} style={{ color: "#356277", fontSize: 80 }} />
+                                {/* <DevicesOutlined style={{ color: "#356277", fontSize: 80 }} /> */}
+                                <h2>{mySkills[index].title}</h2>
+                                <p>{mySkills[index].description}</p>
+                            </Paper>
+                        </Grid>
+                    ))}
+
                 </Grid>
             </div>
         </div >
